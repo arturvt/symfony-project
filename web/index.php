3260a56e17061a4c0f1118df06c7eeec44570214
Artur Tenorio <arturvt@gmail.com>
2014-08-26 09:51:28 -0300

    Alterações 01
    Signed-off-by: Artur Tenorio <arturvt@gmail.com>

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+// echo '<h1>Yehaaa</h1>';
+
+use Symfony\Component\HttpFoundation\Response;
+$response = new Response();
+$response->setContent('<html><body><h1>Hello world!</h1></body></html>');
+$response->setStatusCode(Response::HTTP_OK);
+$response->headers->set('Content-Type', 'text/html');
+// prints the HTTP headers followed by the content
+$response->send();
