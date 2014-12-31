e7eafd198e37e8f1ef6f04e76145ba0d3b9c2902
Artur Tenorio <artur_tenorio@apple.com>
2014-12-31 09:57:12 -0300

    Removing AcmeBundles
    Signed-off-by: Artur Tenorio <artur_tenorio@apple.com>

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Acme\TestBundle\AcmeTestBundle(),
-            new Acme\BlogBundle\AcmeBlogBundle(),
             new Blog\BaseCRUDBundle\BlogBaseCRUDBundle(),
             new Blog\APIBundle\BlogAPIBundle(),
             new Blog\DoctrineExceptionBundle\DoctrineExceptionBundle(),
@@ -27,7 +25,6 @@ class AppKernel extends Kernel
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
